137b4003c4823d00d0d1fe742840c315a35c63cc
Genesio-git <genesioftj04@gmail.com>
2025-12-05 23:44:15 -0300

    refactor: reorganização da estrutura de eventos
    Foram removidos os arquivos create, delete e edit.
    O CRUD foi centralizado no form.html

--- src/apps/events/urls.py
+++ src/apps/events/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='list-events'),
-    path('create/', views.create_event, name='create-event'),
-    path('edit/<int:pk>/', views.edit_event, name='edit-event'),
-    path('delete/<int:pk>/', views.delete_event, name='delete-event'),
+    path('', views.list_events, name="list-events"),
+    path('create/', views.create_event, name="create-event"),
+    path('<int:id>/edit/', views.edit_event, name="edit-event"),
+    path('<int:id>/delete/', views.delete_event, name="delete-event"),
+    path('<int:id>/detail/', views.detail_event, name="detail-event"),
 ]
